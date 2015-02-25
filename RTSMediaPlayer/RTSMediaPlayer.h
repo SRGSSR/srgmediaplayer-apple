7afecf732749a73e2295a7272cb2e673465c05d6
Cédric Luthi <cedric.luthi@rts.ch>
2015-02-25 09:49:10 +0100

    Cleanup RTSMediaPlayer.h

--- RTSMediaPlayer/RTSMediaPlayer.h
+++ RTSMediaPlayer/RTSMediaPlayer.h
@@ -1,13 +1,6 @@
 //
-//  RTSMediaPlayer.h
-//  RTSMediaPlayer
-//
 //  Created by Cédric Luthi on 25.02.15.
 //  Copyright (c) 2015 RTS. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
-@interface RTSMediaPlayer : NSObject
-
-@end
