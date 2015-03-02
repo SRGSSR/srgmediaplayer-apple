aa53b73127fcec14b39b95b29fd025a942496a6a
Frédéric Humbert-Droz <fhumbertdroz@clio.ch>
2015-03-02 16:35:42 +0100

    Add new `RTSMediaPlayerLoadStateTestCase`

---
+++ RTSMediaPlayer Tests/RTSMediaPlayerLoadStateTestCase.m
@@ -0,0 +1,18 @@
+//
+//  RTSMediaPlayerLoadStateTestCase.m
+//  RTSMediaPlayer
+//
+//  Created by Frédéric Humbert-Droz on 02/03/15.
+//  Copyright (c) 2015 RTS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <RTSMediaPlayer/RTSMediaPlayer.h>
+
+@interface RTSMediaPlayerLoadStateTestCase : XCTestCase
+
+@end
+
+@implementation RTSMediaPlayerLoadStateTestCase
+
+@end
