1880b11916138353a4292d82cc31d6dde5695699
Frédéric Humbert-Droz <fhumbertdroz@clio.ch>
2015-03-02 16:03:13 +0100

    Add new test case for playbackState update and notification when calling `-play`

--- RTSMediaPlayer Tests/RTSMediaPlayerPlaybackTestCase.m
+++ RTSMediaPlayer Tests/RTSMediaPlayerPlaybackTestCase.m
@@ -5,6 +5,7 @@
 
 #import <XCTest/XCTest.h>
 #import <RTSMediaPlayer/RTSMediaPlayer.h>
+#import "NSObject+KVOBlock.h"
 
 @interface RTSMediaPlayerPlaybackTestCase : XCTestCase
 @property RTSMediaPlayerController *mediaPlayerController;
@@ -76,6 +77,39 @@
 	}];
 	[self.mediaPlayerController play];
 	[self waitForExpectationsWithTimeout:5 handler:nil];
+}
+
+- (void) testMultiplePlayDoesNotUpdatePlaybackStateAndDoesNotSendNotifications
+{
+	__block NSInteger playbackStateKVOChangeCount = 0;
+	[self.mediaPlayerController observeKeyPath:@"playbackState" withBlock:^(RTSMediaPlayerController *mediaPlayerController, NSString *keyPath, NSDictionary *changes) {
+		if (mediaPlayerController.playbackState == RTSMediaPlaybackStatePlaying)
+			playbackStateKVOChangeCount++;
+	}];
+	
+	__block NSInteger playbackStateNotificationChangeCount = 0;
+	[[NSNotificationCenter defaultCenter] addObserverForName:RTSMediaPlayerPlaybackStateDidChangeNotification object:self.mediaPlayerController queue:nil usingBlock:^(NSNotification *notification) {
+		RTSMediaPlayerController *mediaPlayerController = notification.object;
+		if (mediaPlayerController.playbackState == RTSMediaPlaybackStatePlaying)
+			playbackStateNotificationChangeCount++;
+	}];
+	
+	[self expectationForNotification:RTSMediaPlayerPlaybackStateDidChangeNotification object:self.mediaPlayerController handler:^BOOL(NSNotification *notification) {
+		return self.mediaPlayerController.playbackState == RTSMediaPlaybackStatePlaying;
+	}];
+	[self.mediaPlayerController play];
+	[self waitForExpectationsWithTimeout:5 handler:nil];
+	
+	[self.mediaPlayerController play];
+	[self.mediaPlayerController play];
+	
+	[self expectationForNotification:RTSMediaPlayerPlaybackDidFinishNotification object:self.mediaPlayerController handler:^BOOL(NSNotification *notification) {
+		return playbackStateKVOChangeCount == 1 && playbackStateNotificationChangeCount == 1;
+	}];
+	[self.mediaPlayerController stop];
+	[self waitForExpectationsWithTimeout:5 handler:nil];
+	
+	[self.mediaPlayerController unobserveKeyPath:@"playbackState"];
 }
 
 @end
