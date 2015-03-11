17ff92921f5f26858081bf874791d22304d0f6bd
Cédric Luthi <cedric.luthi@rts.ch>
2015-03-11 16:48:37 +0100

    Update demo app pod

--- RTSMediaPlayer Demo/Pods/Target Support Files/Pods/Pods-environment.h
+++ RTSMediaPlayer Demo/Pods/Target Support Files/Pods/Pods-environment.h
@@ -18,3 +18,15 @@
 #define COCOAPODS_VERSION_MINOR_TransitionKit 1
 #define COCOAPODS_VERSION_PATCH_TransitionKit 1
 
+// libextobjc/EXTScope
+#define COCOAPODS_POD_AVAILABLE_libextobjc_EXTScope
+#define COCOAPODS_VERSION_MAJOR_libextobjc_EXTScope 0
+#define COCOAPODS_VERSION_MINOR_libextobjc_EXTScope 4
+#define COCOAPODS_VERSION_PATCH_libextobjc_EXTScope 1
+
+// libextobjc/RuntimeExtensions
+#define COCOAPODS_POD_AVAILABLE_libextobjc_RuntimeExtensions
+#define COCOAPODS_VERSION_MAJOR_libextobjc_RuntimeExtensions 0
+#define COCOAPODS_VERSION_MINOR_libextobjc_RuntimeExtensions 4
+#define COCOAPODS_VERSION_PATCH_libextobjc_RuntimeExtensions 1
+
