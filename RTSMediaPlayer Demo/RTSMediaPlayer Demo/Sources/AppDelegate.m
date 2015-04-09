4a5a2356351234e9cc96dfeae942faa1468f71a0
Cédric Luthi <cedric.luthi@rts.ch>
2015-04-09 16:36:00 +0200

    Basic tty logger in demo app

--- RTSMediaPlayer Demo/RTSMediaPlayer Demo/Sources/AppDelegate.m
+++ RTSMediaPlayer Demo/RTSMediaPlayer Demo/Sources/AppDelegate.m
@@ -5,8 +5,39 @@
 
 #import "AppDelegate.h"
 
+#import <CocoaLumberjack/CocoaLumberjack.h>
+
+
+@interface LogFormatter : NSObject <DDLogFormatter>
+@end
+
+@implementation LogFormatter
+
+- (NSString *) formatLogMessage:(DDLogMessage *)logMessage
+{
+	static NSDateFormatter *dateFormatter;
+	static dispatch_once_t once;
+	dispatch_once(&once, ^{
+		dateFormatter = [NSDateFormatter new];
+		dateFormatter.dateFormat = @"HH:mm:ss.SSS";
+	});
+	return [NSString stringWithFormat:@"%@ [%@] %@", [dateFormatter stringFromDate:logMessage.timestamp], logMessage.threadID, logMessage.message];
+}
+
+@end
+
+
 @implementation AppDelegate
 
 @synthesize window = _window;
+
+- (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+	DDTTYLogger *ttyLogger = [DDTTYLogger sharedInstance];
+	ttyLogger.colorsEnabled = YES;
+	ttyLogger.logFormatter = [LogFormatter new];
+	[DDLog addLogger:ttyLogger];
+	return YES;
+}
 
 @end
