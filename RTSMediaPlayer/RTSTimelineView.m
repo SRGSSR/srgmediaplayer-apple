ae78b6411eaece822875027069a9c56d55aec580
Samuel Défago <defagos@gmail.com>
2015-04-29 08:15:53 +0200

    Draw event dots

--- RTSMediaPlayer/RTSTimelineView.m
+++ RTSMediaPlayer/RTSTimelineView.m
@@ -5,11 +5,12 @@
 
 #import "RTSTimelineView.h"
 
+static const CGFloat RTSTimelineBarHeight = 2.f;
+static const CGFloat RTSTimelineBarMargin = 8.f;
+static const CGFloat RTSTimelineEventViewSide = 8.f;
+
 static void commonInit(RTSTimelineView *self)
 {
-	static const CGFloat RTSTimelineBarHeight = 2.f;
-	static const CGFloat RTSTimelineBarMargin = 8.f;
-	
 	UIView *barView = [[UIView alloc] initWithFrame:CGRectMake(RTSTimelineBarMargin,
 															   roundf((CGRectGetHeight(self.frame) - RTSTimelineBarHeight) / 2.f),
 															   CGRectGetWidth(self.frame) - 2.f * RTSTimelineBarMargin,
@@ -19,11 +20,18 @@ static void commonInit(RTSTimelineView *self)
 	[self addSubview:barView];
 }
 
+@interface RTSTimelineView ()
+
+@property (nonatomic) NSArray *eventViews;
+
+@end
+
 @implementation RTSTimelineView
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
-	if (self = [super initWithFrame:frame]) {
+	if (self = [super initWithFrame:frame])
+	{
 		commonInit(self);
 	}
 	return self;
@@ -31,20 +39,50 @@ static void commonInit(RTSTimelineView *self)
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
-	if (self = [super initWithCoder:aDecoder]) {
+	if (self = [super initWithCoder:aDecoder])
+	{
 		commonInit(self);
 	}
 	return self;
 }
 
+- (void)willMoveToWindow:(UIWindow *)window
+{
+	[super willMoveToWindow:window];
+	
+	if (window)
+	{
+		[self reloadData];
+	}
+}
+
 - (void) reloadData
 {
+	for (UIView *eventView in self.eventViews)
+	{
+		[eventView removeFromSuperview];
+	}
+	
 	NSInteger numberOfEvents = [self.dataSource numberOfEventsInTimelineView:self];
+	
+	NSMutableArray *eventViews = [NSMutableArray array];
 	for (NSInteger i = 0; i < numberOfEvents; ++i)
 	{
-		// RTSTimelineEvent *event = [self.dataSource timelineView:self eventAtIndex:i];
+//		RTSTimelineEvent *event = [self.dataSource timelineView:self eventAtIndex:i];
+		UIView *eventView = [[UIView alloc] initWithFrame:CGRectMake(RTSTimelineBarMargin + 30.f * i,
+																	 roundf((CGRectGetHeight(self.frame) - RTSTimelineEventViewSide) / 2.f),
+																	 RTSTimelineEventViewSide,
+																	 RTSTimelineEventViewSide)];
+		eventView.backgroundColor = [UIColor colorWithWhite:1.f alpha:0.6f];
+		eventView.layer.cornerRadius = RTSTimelineEventViewSide / 2.f;
+		eventView.layer.borderColor = [UIColor blackColor].CGColor;
+		eventView.layer.borderWidth = 1.f;
+		eventView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin| UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+		[self addSubview:eventView];
 		
+		[eventViews addObject:eventView];
 	}
+	self.eventViews = [NSArray arrayWithArray:eventViews];
 }
 
 @end
