0bd2acc9b0841189d6fed81421a0fc899460ce28
Samuel Défago <defagos@gmail.com>
2015-04-29 07:53:30 +0200

    Display timeline bar

--- RTSMediaPlayer/RTSTimelineView.m
+++ RTSMediaPlayer/RTSTimelineView.m
@@ -5,7 +5,37 @@
 
 #import "RTSTimelineView.h"
 
+static void commonInit(RTSTimelineView *self)
+{
+	static const CGFloat RTSTimelineBarHeight = 2.f;
+	static const CGFloat RTSTimelineBarMargin = 8.f;
+	
+	UIView *barView = [[UIView alloc] initWithFrame:CGRectMake(RTSTimelineBarMargin,
+															   roundf((CGRectGetHeight(self.frame) - RTSTimelineBarHeight) / 2.f),
+															   CGRectGetWidth(self.frame) - 2.f * RTSTimelineBarMargin,
+															   RTSTimelineBarHeight)];
+	barView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleWidth;
+	barView.backgroundColor = [UIColor whiteColor];
+	[self addSubview:barView];
+}
+
 @implementation RTSTimelineView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+	if (self = [super initWithFrame:frame]) {
+		commonInit(self);
+	}
+	return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+	if (self = [super initWithCoder:aDecoder]) {
+		commonInit(self);
+	}
+	return self;
+}
 
 - (void) reloadData
 {
