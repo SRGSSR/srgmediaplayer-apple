5f785657176df5eb6bdb7db873b174b36ec2df71
Samuel Défago <defagos@gmail.com>
2015-04-30 10:29:46 +0200

    Use boundary observer to receive clean events from a periodic observer

--- RTSMediaPlayer/RTSPlaybackBlockRegistration.m
+++ RTSMediaPlayer/RTSPlaybackBlockRegistration.m
@@ -17,8 +17,6 @@
 @property (nonatomic) id playbackStartObserver;
 @property (nonatomic) id periodicTimeObserver;
 
-@property (nonatomic, getter=isPlaybackStarted) BOOL playbackStarted;
-
 @end
 
 @implementation RTSPlaybackBlockRegistration
@@ -42,37 +40,56 @@
 	
 	self.player = player;
 	
+	[self resetObservers];
+}
+
+- (void) resetObservers
+{
+	[self removeObservers];
+	
 	CMTime startTime = CMTimeAdd(self.player.currentItem.currentTime, CMTimeMake(1., 10.));
 	
-	@weakify(self, player)
-	self.playbackStartObserver = [player addBoundaryTimeObserverForTimes:@[[NSValue valueWithCMTime:startTime]] queue:NULL usingBlock:^{
-		@strongify(self, player)
+	@weakify(self)
+	self.playbackStartObserver = [self.player addBoundaryTimeObserverForTimes:@[[NSValue valueWithCMTime:startTime]] queue:NULL usingBlock:^{
+		@strongify(self)
 		
-		self.playbackStarted = YES;
-		
-		[player removeTimeObserver:self.playbackStartObserver];
+		[self.player removeTimeObserver:self.playbackStartObserver];
 		self.playbackStartObserver = nil;
-	}];
-	
-	self.periodicTimeObserver = [player addPeriodicTimeObserverForInterval:self.interval queue:NULL usingBlock:^(CMTime time) {
-		if (!self.playbackStarted)
-		{
-			return;
-		}
 		
-		self.playbackBlock ? self.playbackBlock(time) : nil;
+		@weakify(self)
+		self.periodicTimeObserver = [self.player addPeriodicTimeObserverForInterval:self.interval queue:NULL usingBlock:^(CMTime time) {
+			@strongify(self)
+			
+			if (self.player.rate == 0.)
+			{
+				[self resetObservers];
+				return;
+			}
+			
+			self.playbackBlock ? self.playbackBlock(time) : nil;
+		}];
 	}];
+}
+
+- (void) removeObservers
+{
+	if (self.playbackStartObserver)
+	{
+		[self.player removeTimeObserver:self.playbackStartObserver];
+		self.playbackStartObserver = nil;
+	}
+	
+	if (self.periodicTimeObserver)
+	{
+		[self.player removeTimeObserver:self.periodicTimeObserver];
+		self.periodicTimeObserver = nil;
+	}
 }
 
 - (void) unregister
 {
-	[self.player removeTimeObserver:self.playbackStartObserver];
-	[self.player removeTimeObserver:self.periodicTimeObserver];
+	[self removeObservers];
 	
-	self.playbackStartObserver = nil;
-	self.periodicTimeObserver = nil;
-	
-	self.playbackStarted = NO;
 	self.player = nil;
 }
 
