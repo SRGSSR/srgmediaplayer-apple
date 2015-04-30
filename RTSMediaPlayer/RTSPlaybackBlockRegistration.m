95cc896ebe80a5ba35d50620480bb03a17559c98
Samuel Défago <defagos@gmail.com>
2015-04-30 08:46:10 +0200

    Use a boundary time observer to filter out events received from the periodic observer when playback starts

--- RTSMediaPlayer/RTSPlaybackBlockRegistration.m
+++ RTSMediaPlayer/RTSPlaybackBlockRegistration.m
@@ -5,13 +5,19 @@
 
 #import "RTSPlaybackBlockRegistration.h"
 
+#import <libextobjc/EXTScope.h>
+
 @interface RTSPlaybackBlockRegistration ()
 
 @property (nonatomic, copy) void (^playbackBlock)(CMTime time);
 @property (nonatomic) CMTime interval;
 
 @property (nonatomic, weak) AVPlayer *player;
+
+@property (nonatomic) id playbackStartObserver;
 @property (nonatomic) id periodicTimeObserver;
+
+@property (nonatomic, getter=isPlaybackStarted) BOOL playbackStarted;
 
 @end
 
@@ -35,12 +41,38 @@
 	}
 	
 	self.player = player;
-	self.periodicTimeObserver = [player addPeriodicTimeObserverForInterval:self.interval queue:dispatch_get_main_queue() usingBlock:self.playbackBlock];
+	
+	CMTime startTime = CMTimeAdd(self.player.currentItem.currentTime, CMTimeMake(1., 10.));
+	
+	@weakify(self, player)
+	self.playbackStartObserver = [player addBoundaryTimeObserverForTimes:@[[NSValue valueWithCMTime:startTime]] queue:NULL usingBlock:^{
+		@strongify(self, player)
+		
+		self.playbackStarted = YES;
+		
+		[player removeTimeObserver:self.playbackStartObserver];
+		self.playbackStartObserver = nil;
+	}];
+	
+	self.periodicTimeObserver = [player addPeriodicTimeObserverForInterval:self.interval queue:NULL usingBlock:^(CMTime time) {
+		if (!self.playbackStarted)
+		{
+			return;
+		}
+		
+		self.playbackBlock ? self.playbackBlock(time) : nil;
+	}];
 }
 
 - (void) unregister
 {
+	[self.player removeTimeObserver:self.playbackStartObserver];
 	[self.player removeTimeObserver:self.periodicTimeObserver];
+	
+	self.playbackStartObserver = nil;
+	self.periodicTimeObserver = nil;
+	
+	self.playbackStarted = NO;
 	self.player = nil;
 }
 
