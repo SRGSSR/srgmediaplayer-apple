653f791e47d77d24127d8af27b0adcc7102f85b2
Samuel Défago <defagos@gmail.com>
2015-05-01 08:19:34 +0200

    Skip events not in the timeline

--- RTSMediaPlayer/RTSTimelineView.m
+++ RTSMediaPlayer/RTSTimelineView.m
@@ -101,6 +101,12 @@ static void commonInit(RTSTimelineView *self)
 	for (NSInteger i = 0; i < numberOfEvents; ++i)
 	{
 		RTSTimelineEvent *event = [self.dataSource timelineView:self eventAtIndex:i];
+		
+		// Skip events not in the timeline
+		if (CMTIME_COMPARE_INLINE(event.time, >, CMTimeRangeGetEnd(currentTimeRange))) {
+			continue;
+		}
+		
 		UIView *eventView = [[UIView alloc] initWithFrame:CGRectMake(roundf(RTSTimelineBarMargin + CMTimeGetSeconds(event.time) * (CGRectGetWidth(self.frame) - 2.f * RTSTimelineBarMargin) / CMTimeGetSeconds(currentTimeRange.duration) - RTSTimelineEventViewSide / 2.f),
 																	 roundf((CGRectGetHeight(self.frame) - RTSTimelineEventViewSide) / 2.f),
 																	 RTSTimelineEventViewSide,
