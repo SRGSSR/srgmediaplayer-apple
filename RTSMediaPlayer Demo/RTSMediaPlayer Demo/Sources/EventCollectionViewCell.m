fbc892e7b2347ba24997ef4c89e9b0ae76b6e594
Samuel Défago <defagos@gmail.com>
2015-05-05 10:09:21 +0200

    Display timestamps

--- RTSMediaPlayer Demo/RTSMediaPlayer Demo/Sources/EventCollectionViewCell.m
+++ RTSMediaPlayer Demo/RTSMediaPlayer Demo/Sources/EventCollectionViewCell.m
@@ -7,9 +7,12 @@
 
 #import <SDWebImage/UIImageView+WebCache.h>
 
+NSString *StringFromTime(CMTime time);
+
 @interface EventCollectionViewCell ()
 
 @property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+@property (nonatomic, weak) IBOutlet UILabel *timestampLabel;
 @property (nonatomic, weak) IBOutlet UIImageView *imageView;
 
 @end
@@ -23,8 +26,33 @@
 	_event = event;
 	
 	self.titleLabel.text = event.title;
+	self.timestampLabel.text = StringFromTime(event.time);
 	
 	[self.imageView sd_setImageWithURL:event.imageURL];
 }
 
 @end
+
+#pragma mark - Functions
+
+NSString *StringFromTime(CMTime time)
+{
+	if (!CMTIME_IS_VALID(time))
+	{
+		return @"--:--";
+	}
+	
+	NSInteger timeInSeconds = CMTimeGetSeconds(time);
+	NSInteger hours = timeInSeconds / (60 * 60);
+	NSInteger minutes = (timeInSeconds - hours * 60 * 60) / 60;
+	NSInteger seconds = timeInSeconds - hours * 60 * 60 - minutes * 60;
+	
+	if (hours > 0)
+	{
+		return [NSString stringWithFormat:@"%02ld:%02ld:%02ld", (long)hours, (long)minutes, (long)seconds];
+	}
+	else
+	{
+		return [NSString stringWithFormat:@"%02ld:%02ld", (long)minutes, (long)seconds];
+	}
+}
