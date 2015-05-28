cc26023cc2352f21d4dae74b80b3537cfe12047b
Cédric Foellmi <cedric@onekilopars.ec>
2015-05-28 15:02:55 +0200

    Real meat inside the segment controller, borrowed almost directly from the existing SRGMediaPlayer

--- RTSMediaPlayer/RTSMediaSegmentsController.h
+++ RTSMediaPlayer/RTSMediaSegmentsController.h
@@ -8,9 +8,73 @@
 
 #import <Foundation/Foundation.h>
 #import "RTSMediaPlayerSegmentDataSource.h"
+#import "RTSMediaPlayerSegment.h"
+#import "RTSMediaPlayerController.h"
 
 @interface RTSMediaSegmentsController : NSObject
 
-@property(nonatomic, weak) id<RTSMediaPlayerSegmentDataSource> dataSource;
+- (instancetype)initWithPlayerController:(RTSMediaPlayerController *)playerController
+                          fullLenghMedia:(id<RTSMediaPlayerSegment>)media
+                                segments:(NSArray<RTSMediaPlayerSegment> *)segments;
+
+/**
+ *  The count of segments.
+ *
+ *  @return An unsigned integer indicating the count of segments.
+ */
+- (NSUInteger)countOfSegments;
+
+/**
+ *  The data sources of each segments.
+ *
+ *  @return A an array containing the data sources of each segment.
+ */
+- (NSArray *)segments;
+
+/**
+ *  The count of visible segments
+ *
+ *  @return An unsigned integer indicating the count of episodes.
+ */
+- (NSUInteger)countOfVisibleSegments;
+
+/**
+ *  The data sources of each episodes.
+ *
+ *  @return A an array containing the data sources of each episodes.
+ */
+- (NSArray *)visibleSegments;
+
+/**
+ *  For each segment index, one must have a given 'visible segment' index.
+ *
+ *  @param segmentIndex The index of the segment.
+ *
+ *  @return The index of the episodes corresponding to the provided segment index.
+ */
+- (NSUInteger)visibleSegmentIndexForSegmentIndex:(NSUInteger)segmentIndex;
+
+/**
+ *  Check whether the segment at the given index is blocked.
+ *
+ *  @param index The index of the segment to be checked.
+ *
+ *  @return YES if the segment is blocked.
+ */
+- (BOOL)isSegmentBlockedAtIndex:(NSInteger)index;
+
+/**
+ *  When hitting a blocked segment, one must find when exactly restarting the video, if possible. Hence,
+ *  given the actual blocked segment at index, we look for the last of the next segments that is also blocked.
+ *  Small gaps in between segments (say below < 0.2 seconds) are considered as non-playable content, and
+ *  the two segments considered as contiguous.
+ *
+ *  @param index          The index of the last contiguous segment that is blocked. It can be equal to the current index.
+ *  @param flexibilityGap A small gap (say ~ 0.1 sec) used for some flexbility for checking contiguity between segment times.
+ *
+ *  @return
+ *  If there is no more segments and no more playable content, returns NSNotFound;
+ */
+- (NSInteger)indexOfLastContiguousBlockedSegmentAfterIndex:(NSInteger)index withFlexibilityGap:(CGFloat)flexibilityGap;
 
 @end
--- RTSMediaPlayer/RTSMediaSegmentsController.m
+++ RTSMediaPlayer/RTSMediaSegmentsController.m
@@ -8,6 +8,138 @@
 
 #import "RTSMediaSegmentsController.h"
 
+@interface RTSMediaSegmentsController () {
+    RTSMediaPlayerController *_playerController;
+    id<RTSMediaPlayerSegment> _media;
+    NSArray *_segments;
+    NSArray *_episodes;
+    NSDictionary *_indexMapping;
+}
+@end
+
 @implementation RTSMediaSegmentsController
+
+- (instancetype)initWithPlayerController:(RTSMediaPlayerController *)playerController
+                          fullLenghMedia:(id<RTSMediaPlayerSegment>)media
+                                segments:(NSArray<RTSMediaPlayerSegment> *)segments
+{
+    NSParameterAssert(playerController && media && segments);
+    
+    self = [super init];
+    if (self) {
+        _playerController = playerController;
+        _media = media;
+        _segments = segments;
+        
+        NSMutableArray *episodes = [NSMutableArray array];
+        NSMutableIndexSet *blockedIndices = [NSMutableIndexSet indexSet];
+        NSMutableDictionary *indexMapping = [NSMutableDictionary dictionary];
+        
+        __block NSInteger episodeIndex = -1;
+        [_segments enumerateObjectsUsingBlock:^(id<RTSMediaPlayerSegment>segment, NSUInteger idx, BOOL *stop) {
+            if ([segment isBlocked] == NO) {
+                [blockedIndices addIndex:idx];
+            }
+            if ([segment isVisible]) {
+                episodeIndex ++;
+                [indexMapping setObject:@(episodeIndex) forKey:@(idx)];
+                [episodes addObject:segment];
+            }
+            else {
+                [indexMapping setObject:@(NSNotFound) forKey:@(idx)];
+            }
+        }];
+        
+        _episodes = [NSArray arrayWithArray:episodes]; // Ensure there is always an array, even if it is empty.
+        _indexMapping = [indexMapping copy];
+        
+        NSAssert(_indexMapping.count == _segments.count,
+                 @"One must have the same number of index for mapping as we have segments.");
+    }
+    return self;
+}
+
+- (NSUInteger)countOfSegments
+{
+    return _segments.count;
+}
+
+- (NSArray *)segments
+{
+    return [NSArray arrayWithArray:_segments];
+}
+
+- (NSUInteger)countOfVisibleSegments
+{
+    return _episodes.count;
+}
+
+- (NSArray *)visibleSegments
+{
+    return [NSArray arrayWithArray:_episodes];
+}
+
+- (NSUInteger)visibleSegmentIndexForSegmentIndex:(NSUInteger)segmentIndex
+{
+    if (segmentIndex == NSNotFound || _segments.count == 0) {
+        return NSNotFound;
+    }
+    return [[_indexMapping objectForKey:@(segmentIndex)] unsignedIntegerValue];
+}
+
+- (BOOL)isSegmentBlockedAtIndex:(NSInteger)index
+{
+    if (index >= 0 && index < _segments.count) {
+        return [_segments[index] isBlocked];
+    }
+    return NO;
+}
+
+- (NSInteger)indexOfLastContiguousBlockedSegmentAfterIndex:(NSInteger)index withFlexibilityGap:(CGFloat)flexibilityGap
+{
+    if (_segments.count == 0 || index == NSNotFound || index >= _segments.count) {
+        return NSNotFound;
+    }
+    
+    id<RTSMediaPlayerSegment> inputSegment = _segments[index];
+    NSTimeInterval inputSegmentEndTime = (NSTimeInterval) CMTimeGetSeconds(inputSegment.segmentTimeRange.start)+CMTimeGetSeconds(inputSegment.segmentTimeRange.duration);
+
+    if (index+1 >= _segments.count) {
+        // Ok, we have no additional segments  See if there is some time left at end of full length media.
+        
+        NSTimeInterval fullLengthEndTime = (NSTimeInterval) CMTimeGetSeconds(_media.segmentTimeRange.start)+CMTimeGetSeconds(_media.segmentTimeRange.duration);
+        NSTimeInterval mediaLastSeconds = fullLengthEndTime - inputSegmentEndTime;
+        
+        if (mediaLastSeconds < 2*flexibilityGap) {
+            // There is no meaningful playable content after the end of the current segment.
+            return NSNotFound;
+        }
+        else {
+            // There is some meaningful playable content after the end of the current segment. But no more segment afterwards.
+            // Hence restart playing from the end of the current index.
+            return index;
+        }
+    }
+    
+    NSInteger result = index;
+    for (NSInteger i = index+1; i < _segments.count; i++) {
+        id<RTSMediaPlayerSegment> segment = _segments[i];
+        if (![segment isBlocked]) {
+            // Next segment is not blocked. Hence the end of the current one is the last we are look for.
+            break;
+        }
+        
+        NSTimeInterval gap = (NSTimeInterval) CMTimeGetSeconds(segment.segmentTimeRange.start) - inputSegmentEndTime;
+        if (gap > 2*flexibilityGap) {
+            // Gap is larger than threshold, hence valid, hence playable.
+            break;
+        }
+        
+        // Gap is smaller than threshold, and next segment is blocked. Hence pursue the 'for loop'.
+        result = i;
+    }
+    
+    return result;
+}
 
 @end
