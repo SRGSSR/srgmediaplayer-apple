999922c6957b5b2189202f4180bf07ef8b9e37f4
Cédric Foellmi <cedric@onekilopars.ec>
2015-06-02 10:34:51 +0200

    Much simplifying the segments demo

--- RTSMediaPlayer Demo/RTSMediaPlayer Demo/Sources/PseudoILDataProvider.m
+++ RTSMediaPlayer Demo/RTSMediaPlayer Demo/Sources/PseudoILDataProvider.m
@@ -20,68 +20,13 @@ static NSString * const SRGILTokenHandlerBaseURLString = @"http://tp.srgssr.ch/t
 	   contentURLForIdentifier:(NSString *)identifier
 			 completionHandler:(void (^)(NSURL *, NSError *))completionHandler
 {
-	NSString *businessUnit = nil;
-	NSString *trueIdentifier = nil;
-	if ([identifier isEqualToString:@"srf-0"]) {
-		businessUnit = @"srf";
-		trueIdentifier = @"e6657156-859c-414f-a3be-b18c12ccd3d7";
+	if ([identifier isEqualToString:@"error"]) {
+		completionHandler(nil, [NSError errorWithDomain:@"Demo" code:123456 userInfo:@{NSLocalizedDescriptionKey: @"error"}]);
 	}
-	
-	NSString *URLString = [NSString stringWithFormat:@"http://il.srgssr.ch/integrationlayer/1.0/ue/%@/video/play/%@.json",
-						   businessUnit, trueIdentifier];
-	
-	[NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:URLString]]
-									   queue:[NSOperationQueue mainQueue]
-						   completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-							   if (error) {
-								   completionHandler(nil, error);
-								   return;
-							   }
-							   
-							   NSDictionary *video = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
-							   if (!video) {
-								   NSError *e = [NSError errorWithDomain:@"Demo"
-																	code:123456
-																userInfo:@{NSLocalizedDescriptionKey: @"Can't create video object"}];
-								   
-								   completionHandler(nil, e);
-								   return;
-							   }
-							   
-							   NSString *URLString = nil;
-							   NSArray *playlists = video[@"Video"][@"Playlists"][@"Playlist"];
-							   for (NSDictionary *playlist in playlists) {
-								   if ([[playlist objectForKey:@"@protocol"] isEqualToString:@"HTTP-HLS"]) {
-									   URLString = [[playlist[@"url"] firstObject] objectForKey:@"text"];
-									   break;
-								   }
-							   }
-							   
-							   if (!URLString) {
-								   NSError *e = [NSError errorWithDomain:@"Demo"
-																	code:123456
-																userInfo:@{NSLocalizedDescriptionKey: @"Can't find URL in video object"}];
-								   completionHandler(nil, e);
-							   }
-							   
-							   NSURL *tokenURL = [self tokenRequestURLForURL:[NSURL URLWithString:URLString]];
-							   
-							   // SYNC network request
-							   NSString *JSONString = [NSString stringWithContentsOfURL:tokenURL
-																			   encoding:NSUTF8StringEncoding
-																				  error:&error];
-							   
-							   NSError *deserializationError = nil;
-							   NSDictionary *JSON = [NSJSONSerialization JSONObjectWithData:[JSONString dataUsingEncoding:NSUTF8StringEncoding]
-																					options:0
-																					  error:&deserializationError];
-							   
-							   NSMutableString *finalURLString = [URLString mutableCopy];
-							   [finalURLString appendString:@"?"];
-							   [finalURLString appendString:[JSON[@"token"] objectForKey:@"authparams"]];
-							   
-							   completionHandler([NSURL URLWithString:finalURLString], nil);
-						   }];
+	else {
+		// 19:30 RTS, token unnecessary. Length: 31:57.
+		completionHandler([NSURL URLWithString:@"http://rtsch-i.akamaihd.net/i/tj/2015/tj_20150601_full_f_859152-,101,701,1201,k.mp4.csmil/master.m3u8"], nil);
+	}	
 }
 
 - (NSURL *)tokenRequestURLForURL:(NSURL *)url
