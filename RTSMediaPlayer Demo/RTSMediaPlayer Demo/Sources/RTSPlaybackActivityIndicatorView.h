73ea3d112bfab2c9b46802c761a80a05c79694f5
Cédric Foellmi <cedric@onekilopars.ec>
2015-06-10 10:20:07 +0200

    Adding activity view spinner

---
+++ RTSMediaPlayer Demo/RTSMediaPlayer Demo/Sources/RTSPlaybackActivityIndicatorView.h
@@ -0,0 +1,13 @@
+//
+//  RTSPlaybackActivityIndicatorView.h
+//  RTSMediaPlayer Demo
+//
+//  Created by Cédric Foellmi on 10/06/15.
+//  Copyright (c) 2015 RTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTSPlaybackActivityIndicatorView : UIActivityIndicatorView
+
+@end
---
+++ RTSMediaPlayer Demo/RTSMediaPlayer Demo/Sources/RTSPlaybackActivityIndicatorView.m
@@ -0,0 +1,36 @@
+//
+//  RTSPlaybackActivityIndicatorView.m
+//  RTSMediaPlayer Demo
+//
+//  Created by Cédric Foellmi on 10/06/15.
+//  Copyright (c) 2015 RTS. All rights reserved.
+//
+
+#import "RTSPlaybackActivityIndicatorView.h"
+#import <RTSMediaPlayer/RTSMediaPlayer.h>
+
+@implementation RTSPlaybackActivityIndicatorView
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+	self = [super initWithCoder:aDecoder];
+	if (self) {
+		[[NSNotificationCenter defaultCenter] addObserver:self
+												 selector:@selector(updateUponPlaybackStateChange:)
+													 name:RTSMediaPlayerPlaybackStateDidChangeNotification
+												   object:nil];
+	}
+	return self;
+}
+
+- (void)updateUponPlaybackStateChange:(NSNotification *)notif
+{
+	RTSMediaPlayerController *controller = notif.object;
+	BOOL visible = (controller.playbackState == RTSMediaPlaybackStatePreparing ||
+					controller.playbackState == RTSMediaPlaybackStateStalled ||
+					controller.playbackState == RTSMediaPlaybackStateSeeking);
+	
+	self.hidden = !visible;
+}
+
+@end
