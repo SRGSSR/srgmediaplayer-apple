baebd91c6ee95a8d76ffacc89e13f13b6cbf2e96
Samuel Défago <defagos@gmail.com>
2015-06-18 08:15:39 +0200

    Add source files for segment unit tests

---
+++ RTSMediaPlayer Tests/RTSMediaSegmentsTestCase.m
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) RTS. All rights reserved.
+//
+//  Licence information is available from the LICENCE file.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RTSMediaSegmentsTestCase : XCTestCase
+
+@end
+
+@implementation RTSMediaSegmentsTestCase
+
+@end
