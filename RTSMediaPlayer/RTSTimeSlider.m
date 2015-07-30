a8cc80fa95ad1c2b85df271c4d76d0b3566bd10c
Cédric Foellmi <cedric@onekilopars.ec>
2015-07-30 14:53:31 +0200

    Avoiding... invalid range! In addition to empty and indefinite...

--- RTSMediaPlayer/RTSTimeSlider.m
+++ RTSMediaPlayer/RTSTimeSlider.m
@@ -356,7 +356,7 @@ static NSString *RTSTimeSliderFormatter(NSTimeInterval seconds)
 			if (!self.isTracking)
 			{
 				CMTimeRange timeRange = [self.playbackController timeRange];
-				if (!CMTIMERANGE_IS_EMPTY(timeRange) && !CMTIMERANGE_IS_INDEFINITE(timeRange))
+				if (!CMTIMERANGE_IS_EMPTY(timeRange) && !CMTIMERANGE_IS_INDEFINITE(timeRange) && !CMTIMERANGE_IS_INVALID(timeRange))
 				{
 					self.minimumValue = CMTimeGetSeconds(timeRange.start);
 					self.maximumValue = CMTimeGetSeconds(CMTimeRangeGetEnd(timeRange));
