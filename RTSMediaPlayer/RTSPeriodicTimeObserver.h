0f436a6345ef9619d3009230d496108b17fec9d3
Cédric Foellmi <cedric@onekilopars.ec>
2015-07-30 14:46:54 +0200

    Style

--- RTSMediaPlayer/RTSPeriodicTimeObserver.h
+++ RTSMediaPlayer/RTSPeriodicTimeObserver.h
@@ -18,7 +18,7 @@
  *  @param interval    The interval at which the block must be executed
  *  @param queue	   The serial queue onto which block should be enqueued (main queue if NULL)
  */
-- (instancetype) initWithInterval:(CMTime)interval queue:(dispatch_queue_t)queue NS_DESIGNATED_INITIALIZER OS_NONNULL_ALL;
+- (instancetype)initWithInterval:(CMTime)interval queue:(dispatch_queue_t)queue NS_DESIGNATED_INITIALIZER OS_NONNULL_ALL;
 
 /**
  *  Register a block for a given identifier. If a block with the same identifier has already been registered, it
@@ -27,33 +27,33 @@
  *  @param block      The block to register (mandatory)
  *  @param identifier The identifier to which the block must be associated (mandatory)
  */
-- (void) setBlock:(void (^)(CMTime time))block forIdentifier:(NSString *)identifier;
+- (void)setBlock:(void (^)(CMTime time))block forIdentifier:(NSString *)identifier;
 
 /**
  *  Unregister the block matching the provided identifier (does nothing if the identifier is not found)
  *
  *  @param identifier The identifier for which a block must be discarded (mandatory)
  */
-- (void) removeBlockWithIdentifier:(id)identifier;
+- (void)removeBlockWithIdentifier:(id)identifier;
 
 /**
  *  The time interval at which the observer executes all associated blocks
  */
-@property (nonatomic, readonly) CMTime interval;
+@property(nonatomic, readonly) CMTime interval;
 
 /**
  *  The player to which the time observer has been attached, nil if none
  */
-@property (nonatomic, readonly, weak) AVPlayer *player;
+@property(nonatomic, readonly, weak) AVPlayer *player;
 
 /**
  *  Attach to a player. If a previous association existed, it will be removed first
  */
-- (void) attachToMediaPlayer:(AVPlayer *)player;
+- (void)attachToMediaPlayer:(AVPlayer *)player;
 
 /**
  *  Detach the time observer from the associated player, if any
  */
-- (void) detach;
+- (void)detachFromMediaPlayer;
 
 @end
