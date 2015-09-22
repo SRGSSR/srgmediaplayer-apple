dce216004f237a28ff3dabf375c736e74e700268
Cédric Foellmi <cedric@onekilopars.ec>
2015-09-22 10:41:05 +0200

    CocoaPods 0.38

--- Pods/Target Support Files/Pods-SRGMediaPlayerTests-TransitionKit/Pods-SRGMediaPlayerTests-TransitionKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SRGMediaPlayerTests_TransitionKit : NSObject
-@end
-@implementation PodsDummy_Pods_SRGMediaPlayerTests_TransitionKit
-@end
