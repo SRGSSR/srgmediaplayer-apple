71cfcb60e0a6e33041db943896e829bab193971e
Samuel Défago <defagos@gmail.com>
2015-11-03 09:25:48 +0100

    Create reusable PIP button class

---
+++ RTSMediaPlayer/RTSPictureInPictureButton.h
@@ -0,0 +1,32 @@
+//
+//  Copyright (c) SRG. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <UIKit/UIKit.h>
+
+// Forward declarations
+@class RTSMediaPlayerController;
+
+/**
+ *  Standard button images for starting and stopping picture in picture
+ */
+FOUNDATION_EXTERN UIImage *RTSPictureInPictureButtonStartImage(void);
+FOUNDATION_EXTERN UIImage *RTSPictureInPictureButtonStopImage(void);
+
+/**
+ *  A button to toggle picture in picture (if available) for the associated player. This class is not meant to be
+ *  subclassed
+ *
+ *  Simply install an instance somewhere onto your custom player interface and bind to the media player controller which
+ *  the button must be associated with
+ */
+@interface RTSPictureInPictureButton : UIButton
+
+/**
+ *  The media player to which the PIP button must be associated with.
+ */
+@property (nonatomic, weak) IBOutlet RTSMediaPlayerController *mediaPlayerController;
+
+@end
