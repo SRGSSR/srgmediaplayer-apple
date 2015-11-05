ae0d29e29a3a1ae19c9c3c41a5d826fea2760019
Samuel Défago <defagos@gmail.com>
2015-11-05 07:54:13 +0100

    Indent using tabs

--- RTSMediaPlayer/RTSMediaPlayerSharedController.m
+++ RTSMediaPlayer/RTSMediaPlayerSharedController.m
@@ -12,12 +12,12 @@
 
 - (void)pictureInPictureController:(AVPictureInPictureController *)pictureInPictureController restoreUserInterfaceForPictureInPictureStopWithCompletionHandler:(void (^)(BOOL))completionHandler
 {
-    // If no RTSMediaPlayerViewController instance is currently displayed (always modally)
+	// If no RTSMediaPlayerViewController instance is currently displayed (always modally)
 	if (! self.currentViewController) {
 		RTSMediaPlayerViewController *mediaPlayerViewController = [[RTSMediaPlayerViewController alloc] initWithContentIdentifier:self.identifier dataSource:self.dataSource];
 		UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
-        
-        // Dismiss any modal currently displayed if needed
+		
+		// Dismiss any modal currently displayed if needed
 		if (rootViewController.presentedViewController) {
 			[rootViewController dismissViewControllerAnimated:YES completion:^{
 				[rootViewController presentViewController:mediaPlayerViewController animated:YES completion:nil];
@@ -32,8 +32,8 @@
 
 - (void)pictureInPictureControllerDidStopPictureInPicture:(AVPictureInPictureController *)pictureInPictureController
 {
-    // Reset the status of the player when picture in picture is exited anywhere except from the RTSMediaPlayerViewController
-    // itself
+	// Reset the status of the player when picture in picture is exited anywhere except from the RTSMediaPlayerViewController
+	// itself
 	if (!self.currentViewController) {
 		[self reset];
 	}
@@ -41,12 +41,12 @@
 
 - (AVPictureInPictureController *)pictureInPictureController
 {
-    // Lazily installs itself as delegate, in case the pictue in picture controller gets recreated
-    AVPictureInPictureController *pictureInPictureController = super.pictureInPictureController;
-    if (!pictureInPictureController.delegate) {
-        pictureInPictureController.delegate = self;
-    }
-    return pictureInPictureController;
+	// Lazily installs itself as delegate, in case the pictue in picture controller gets recreated
+	AVPictureInPictureController *pictureInPictureController = super.pictureInPictureController;
+	if (!pictureInPictureController.delegate) {
+		pictureInPictureController.delegate = self;
+	}
+	return pictureInPictureController;
 }
 
 @end
