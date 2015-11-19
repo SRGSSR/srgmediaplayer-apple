95c78fe82c5b274f034efa3a50fa7a7191b3eb29
Samuel Défago <defagos@gmail.com>
2015-11-19 14:41:21 +0100

    Fix remaining compilation and podspec validation issues

--- RTSMediaPlayer/RTSMediaPlayerVersion.m
+++ RTSMediaPlayer/RTSMediaPlayerVersion.m
@@ -6,15 +6,11 @@
 
 #import "RTSMediaPlayerVersion.h"
 
-#import "RTSMediaPlayerLogger.h"
-
 NSString * const RTSMediaPlayerVersion(void)
 {
 #ifdef RTS_ANALYTICS_VERSION
     return @(OS_STRINGIFY(RTS_MEDIA_PLAYER_VERSION));
 #else
-    RTSMediaPlayerLogWarning(@"No explicit version has been specified, set to 'dev'. Compile the project with a preprocessor "
-                             "macro called RTS_MEDIA_PLAYER_VERSION supplying the version number (without quotes)");
     return @"dev";
 #endif
 }
