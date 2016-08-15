f2c2f5b59c9c4e50306fdefbec493d108356dec4
Samuel Défago <defagos@gmail.com>
2016-08-15 16:19:42 +0200

    Add marketing version

--- Framework/SRGMediaPlayer.h
+++ Framework/SRGMediaPlayer.h
@@ -6,36 +6,23 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for SRGMediaPlayer.
 FOUNDATION_EXPORT double SRGMediaPlayerVersionNumber;
-
-//! Project version string for SRGMediaPlayer.
 FOUNDATION_EXPORT const unsigned char SRGMediaPlayerVersionString[];
 
+#import "RTSAirplayOverlayView.h"
+#import "RTSMediaFailureOverlayView.h"
 #import "RTSMediaPlayerConstants.h"
 #import "RTSMediaPlayerController.h"
 #import "RTSMediaPlayerControllerDataSource.h"
 #import "RTSMediaPlayerError.h"
-#import "RTSMediaPlayerViewController.h"
-
-// Overlay Views
 #import "RTSMediaPlayerPlaybackButton.h"
-#import "RTSAirplayOverlayView.h"
-#import "RTSMediaFailureOverlayView.h"
-#import "RTSPictureInPictureButton.h"
-#import "RTSTimeSlider.h"
-#import "RTSVolumeView.h"
-#import "RTSPlaybackActivityIndicatorView.h"
-
-// Segments
+#import "RTSMediaPlayerViewController.h"
 #import "RTSMediaSegment.h"
 #import "RTSMediaSegmentsController.h"
 #import "RTSMediaSegmentsDataSource.h"
-
-// Overlay Views for Segments
-#import "RTSTimelineSlider.h"
+#import "RTSPictureInPictureButton.h"
+#import "RTSPlaybackActivityIndicatorView.h"
 #import "RTSSegmentedTimelineView.h"
-
-// Utils
-#import "NSBundle+RTSMediaPlayer.h"
-#import "RTSActivityGestureRecognizer.h"
+#import "RTSTimelineSlider.h"
+#import "RTSTimeSlider.h"
+#import "RTSVolumeView.h"
