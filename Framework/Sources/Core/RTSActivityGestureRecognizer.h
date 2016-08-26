b7dfb3ca44b0662698069ecab61e70ecc6e934b5
Samuel Défago <defagos@gmail.com>
2016-08-26 09:26:58 +0200

    Review files and replace tabs with spaces

--- Framework/Sources/Core/RTSActivityGestureRecognizer.h
+++ Framework/Sources/Core/RTSActivityGestureRecognizer.h
@@ -6,9 +6,13 @@
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 /**
  *  Gesture recognizer which detects all kinds of user activities and call the associated action on its
  *  target if any activity is detected
  */
 @interface RTSActivityGestureRecognizer : UIGestureRecognizer
 @end
+
+NS_ASSUME_NONNULL_END
--- Framework/Sources/Core/RTSActivityGestureRecognizer.m
+++ Framework/Sources/Core/RTSActivityGestureRecognizer.m
@@ -11,50 +11,54 @@
 // Heavily "inspired" by MPActivityGestureRecognizer from the MediaPlayer framework
 @implementation RTSActivityGestureRecognizer
 
-- (instancetype) initWithTarget:(id)target action:(SEL)action
+#pragma mark Object lifecycle
+
+- (instancetype)initWithTarget:(id)target action:(SEL)action
 {
-	if (!(self = [super initWithTarget:target action:action]))
-		return nil;
-	
-	self.cancelsTouchesInView = NO;
-	self.delaysTouchesEnded = NO;
-	
-	return self;
+    if (self = [super initWithTarget:target action:action]) {
+        self.cancelsTouchesInView = NO;
+        self.delaysTouchesEnded = NO;
+    }
+    return self;
 }
 
-- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+#pragma mark UIGestureRecognizer subclassing hooks
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-	[self stopReportingOngoingActivity];
-	[self reportOngoingActivity];
+    [self stopReportingOngoingActivity];
+    [self reportOngoingActivity];
 }
 
-- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
-	self.state = UIGestureRecognizerStateChanged;
+    self.state = UIGestureRecognizerStateChanged;
 }
 
-- (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-	// The MPActivityGestureRecognizer does it a bit differently, see -[MPActivityGestureRecognizer _touchesTerminated:withEvent:]
-	[self stopReportingOngoingActivity];
-	self.state = UIGestureRecognizerStateEnded;
+    // The MPActivityGestureRecognizer does it a bit differently, see -[MPActivityGestureRecognizer _touchesTerminated:withEvent:]
+    [self stopReportingOngoingActivity];
+    self.state = UIGestureRecognizerStateEnded;
 }
 
-- (void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
-	[self stopReportingOngoingActivity];
-	self.state = UIGestureRecognizerStateCancelled;
+    [self stopReportingOngoingActivity];
+    self.state = UIGestureRecognizerStateCancelled;
 }
 
-- (void) reportOngoingActivity
+#pragma mark Helpers
+
+- (void)reportOngoingActivity
 {
-	self.state = UIGestureRecognizerStateBegan;
-	[self performSelector:_cmd withObject:nil afterDelay:1];
+    self.state = UIGestureRecognizerStateBegan;
+    [self performSelector:_cmd withObject:nil afterDelay:1.];
 }
 
-- (void) stopReportingOngoingActivity
+- (void)stopReportingOngoingActivity
 {
-	[NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(reportOngoingActivity) object:nil];
+    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(reportOngoingActivity) object:nil];
 }
 
 @end
