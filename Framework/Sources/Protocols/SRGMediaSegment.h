3df9061b687d02dc0a6f6b7630a7739dcd1762bb
Samuel Défago <defagos@gmail.com>
2016-08-30 08:01:10 +0200

    Reduce segment protocol interface

---
+++ Framework/Sources/Protocols/SRGMediaSegment.h
@@ -0,0 +1,19 @@
+//
+//  Copyright (c) SRG. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <CoreMedia/CoreMedia.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol SRGMediaSegment <NSObject>
+
+@property (nonatomic, readonly) CMTimeRange timeRange;
+@property (nonatomic, readonly, getter=isBlocked) BOOL blocked;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Framework/Sources/Segments/RTSMediaSegmentsController.m
+++ Framework/Sources/Segments/RTSMediaSegmentsController.m
@@ -35,53 +35,53 @@ NSString *const SRGMediaPlaybackSegmentChangeUserSelectInfoKey = @"SRGMediaPlayb
 - (void)setPlayerController:(SRGMediaPlayerController *)playerController
 {
     _playerController = playerController;
-
+    
     playerController.segmentsController = self;
 }
 
 - (void)reloadSegmentsForIdentifier:(NSString *)identifier completionHandler:(void (^)(NSError *error))completionHandler
 {
     NSParameterAssert(identifier);
-
+    
     self.identifier = identifier;
-
+    
     if (! self.playerController) {
         @throw [NSException exceptionWithName:NSInternalInconsistencyException
-                reason:@"Trying to reload data requires to have a player controller."
-                userInfo:nil];
+                                       reason:@"Trying to reload data requires to have a player controller."
+                                     userInfo:nil];
     }
-
+    
     if (! self.dataSource) {
         self.segments = [NSArray new];
     }
-
+    
     [self removeBlockingTimeObserver];
-
+    
     self.lastPlaybackPositionLogicalSegment = nil;
-
+    
     RTSMediaSegmentsCompletionHandler reloadCompletionBlock = ^(NSString *identifier, NSArray *segments, NSError *error) {
         self.segmentsRequestHandle = nil;
-
+        
         if (error) {
             if (completionHandler) {
                 completionHandler(error);
             }
             return;
         }
-
+        
         self.segments = segments;
-
+        
         [self addBlockingTimeObserver];
-
+        
         if (completionHandler) {
             completionHandler(nil);
         }
     };
-
+    
     if (! [self.identifier isEqualToString:identifier] && self.segmentsRequestHandle) {
         [self.dataSource cancelSegmentsRequest:self.segmentsRequestHandle];
     }
-
+    
     self.segmentsRequestHandle = [self.dataSource segmentsController:self segmentsForIdentifier:identifier withCompletionHandler:reloadCompletionBlock];
 }
 
@@ -96,15 +96,15 @@ NSString *const SRGMediaPlaybackSegmentChangeUserSelectInfoKey = @"SRGMediaPlayb
 - (void)addBlockingTimeObserver
 {
     @weakify(self);
-
+    
     // This block will necessarily run on the main thread. See addPeriodicTimeObserverForInterval:... method.
     void (^checkBlock)(CMTime) = ^(CMTime time) {
         @strongify(self);
-
+        
         if (self.playerController.playbackState != SRGPlaybackStatePlaying) {
             return;
         }
-
+        
         // We assume that all logical segments of a full length have an identifier that is IDENTICAL to the fullLength's.
         __block id<RTSMediaSegment> currentSegment = nil;
         [self.segments enumerateObjectsUsingBlock:^(id < RTSMediaSegment > segment, NSUInteger idx, BOOL *_Nonnull stop) {
@@ -115,10 +115,10 @@ NSString *const SRGMediaPlaybackSegmentChangeUserSelectInfoKey = @"SRGMediaPlayb
                 }
             }
         }];
-
+        
         if (self.lastPlaybackPositionLogicalSegment != currentSegment) {
             NSDictionary *userInfo = nil;
-
+            
             if (! currentSegment || (self.lastPlaybackPositionLogicalSegment && currentSegment.blocked)) {
                 userInfo = @{ SRGMediaPlaybackSegmentChangeValueInfoKey: @(RTSMediaPlaybackSegmentEnd),
                               SRGMediaPlaybackSegmentChangePreviousSegmentInfoKey: self.lastPlaybackPositionLogicalSegment,
@@ -135,43 +135,43 @@ NSString *const SRGMediaPlaybackSegmentChangeUserSelectInfoKey = @"SRGMediaPlayb
                               SRGMediaPlaybackSegmentChangeSegmentInfoKey: currentSegment,
                               SRGMediaPlaybackSegmentChangeUserSelectInfoKey: @(NO) };
             }
-
+            
             if (userInfo) {
                 [[NSNotificationCenter defaultCenter] postNotificationName:SRGMediaPlaybackSegmentDidChangeNotification
-                 object:self
-                 userInfo:userInfo];
+                                                                    object:self
+                                                                  userInfo:userInfo];
             }
-
+            
             self.lastPlaybackPositionLogicalSegment = currentSegment;
         }
-
+        
         // Managing blocked segments
         if (currentSegment.blocked) {
             NSDictionary *userInfo = userInfo = @{ SRGMediaPlaybackSegmentChangeValueInfoKey: @(RTSMediaPlaybackSegmentSeekUponBlockingStart),
                                                    SRGMediaPlaybackSegmentChangeSegmentInfoKey: currentSegment };
-
+            
             [[NSNotificationCenter defaultCenter] postNotificationName:SRGMediaPlaybackSegmentDidChangeNotification
-             object:self
-             userInfo:userInfo];
-
+                                                                object:self
+                                                              userInfo:userInfo];
+            
             [self.playerController seekToTime:CMTimeRangeGetEnd(currentSegment.timeRange) completionHandler:^(BOOL finished) {
                 NSDictionary *userInfo = @{ SRGMediaPlaybackSegmentChangeValueInfoKey: @(RTSMediaPlaybackSegmentSeekUponBlockingEnd),
                                             SRGMediaPlaybackSegmentChangePreviousSegmentInfoKey: currentSegment };
                 [[NSNotificationCenter defaultCenter] postNotificationName:SRGMediaPlaybackSegmentDidChangeNotification
-                 object:self
-                 userInfo:userInfo];
-
+                                                                    object:self
+                                                                  userInfo:userInfo];
+                
                 self.lastPlaybackPositionLogicalSegment = nil;
-
+                
                 [self.playerController pause];
             }];
         }
     };
-
+    
     CMTime interval = CMTimeMakeWithSeconds(RTSMediaPlaybackTickInterval, NSEC_PER_SEC);
     self.playerTimeObserver = [self.playerController addPeriodicTimeObserverForInterval:interval
-                               queue:nil
-                               usingBlock:checkBlock];
+                                                                                  queue:nil
+                                                                             usingBlock:checkBlock];
 }
 
 - (NSArray *)visibleSegments
@@ -202,18 +202,18 @@ NSString *const SRGMediaPlaybackSegmentChangeUserSelectInfoKey = @"SRGMediaPlayb
                           SRGMediaPlaybackSegmentChangeSegmentInfoKey: segment,
                           SRGMediaPlaybackSegmentChangeUserSelectInfoKey: @(YES) };
         }
-
+        
         // Immediately send the event. We thus also update the current segment information right here
         self.lastPlaybackPositionLogicalSegment = segment;
-
+        
         [[NSNotificationCenter defaultCenter] postNotificationName:SRGMediaPlaybackSegmentDidChangeNotification
-         object:self
-         userInfo:userInfo];
+                                                            object:self
+                                                          userInfo:userInfo];
     }
     else {
         self.lastPlaybackPositionLogicalSegment = nil;
     }
-
+    
     if ([self.playerController.identifier isEqualToString:segment.segmentIdentifier]) {
         [self.playerController playAtTime:segment.timeRange.start];
     }
