6f630bc4e2857608926022f9ddf68480971d1f7e
Samuel Défago <defagos@gmail.com>
2016-09-30 14:01:14 +0200

    Fix typo

--- Framework/Sources/Core/SRGMediaPlayerLogger.h
+++ Framework/Sources/Core/SRGMediaPlayerLogger.h
@@ -6,8 +6,8 @@
 
 #import "SRGLogger.h"
 
-#define SRGMediaPlayerLogVerbose(category, format, ...) SRGLogVerbose("ch.srgssr.mediaPlayer", category, format, ##__VA_ARGS__)
-#define SRGMediaPlayerLogDebug(category, format, ...)   SRGLogLogDebug("ch.srgssr.mediaPlayer", category, format, ##__VA_ARGS__)
-#define SRGMediaPlayerLogInfo(category, format, ...)    SRGLogInfo("ch.srgssr.mediaPlayer", category, format, ##__VA_ARGS__)
-#define SRGMediaPlayerLogWarning(category, format, ...) SRGLogWarning("ch.srgssr.mediaPlayer", category, format, ##__VA_ARGS__)
-#define SRGMediaPlayerLogError(category, format, ...)   SRGLogError("ch.srgssr.mediaPlayer", category, format, ##__VA_ARGS__)
+#define SRGMediaPlayerLogVerbose(category, format, ...) SRGLogVerbose("ch.srgssr.mediaplayer", category, format, ##__VA_ARGS__)
+#define SRGMediaPlayerLogDebug(category, format, ...)   SRGLogLogDebug("ch.srgssr.mediaplayer", category, format, ##__VA_ARGS__)
+#define SRGMediaPlayerLogInfo(category, format, ...)    SRGLogInfo("ch.srgssr.mediaplayer", category, format, ##__VA_ARGS__)
+#define SRGMediaPlayerLogWarning(category, format, ...) SRGLogWarning("ch.srgssr.mediaplayer", category, format, ##__VA_ARGS__)
+#define SRGMediaPlayerLogError(category, format, ...)   SRGLogError("ch.srgssr.mediaplayer", category, format, ##__VA_ARGS__)
