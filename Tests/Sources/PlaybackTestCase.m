1355f47416bc60781b1241e43d90a60df93dd840
Samuel Défago <defagos@gmail.com>
2017-09-28 14:58:15 +0200

    Use working HTTP Apple stream URLs

--- Tests/Sources/PlaybackTestCase.m
+++ Tests/Sources/PlaybackTestCase.m
@@ -13,7 +13,7 @@
 
 static NSURL *OnDemandTestURL(void)
 {
-    return [NSURL URLWithString:@"https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"];
+    return [NSURL URLWithString:@"http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"];
 }
 
 static NSURL *ShortNonStreamedTestURL(void)
