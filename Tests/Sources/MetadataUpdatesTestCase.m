baa17752275f523c16dd770db71698d37d656ee1
Samuel Défago <defagos@gmail.com>
2017-10-05 10:24:14 +0200

    Add new test suite for metadata updates

---
+++ Tests/Sources/MetadataUpdatesTestCase.m
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MetadataUpdatesTestCase : XCTestCase
+
+@end
+
+@implementation MetadataUpdatesTestCase
+
+@end
