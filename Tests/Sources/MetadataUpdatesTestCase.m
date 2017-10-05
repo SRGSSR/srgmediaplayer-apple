06fd38bd6fd33c7636db539baa97f980993202f8
Samuel Défago <defagos@gmail.com>
2017-10-05 10:54:50 +0200

    Implement segment appearance test

--- Tests/Sources/MetadataUpdatesTestCase.m
+++ Tests/Sources/MetadataUpdatesTestCase.m
@@ -4,17 +4,66 @@
 //  License information is available from the LICENSE file.
 //
 
+#import "Segment.h"
+#import "TestMacros.h"
+#import "XCTestCase+MediaPlayerTests.h"
+
+#import <SRGMediaPlayer/SRGMediaPlayer.h>
 #import <XCTest/XCTest.h>
 
+static NSURL *OnDemandTestURL(void)
+{
+    return [NSURL URLWithString:@"http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"];
+}
+
 @interface MetadataUpdatesTestCase : XCTestCase
+
+@property (nonatomic) SRGMediaPlayerController *mediaPlayerController;
 
 @end
 
 @implementation MetadataUpdatesTestCase
 
+#pragma mark Setup and teardown
+
+- (void)setUp
+{
+    self.mediaPlayerController = [[SRGMediaPlayerController alloc] init];
+}
+
+- (void)tearDown
+{
+    // Always ensure the player gets deallocated between tests
+    [self.mediaPlayerController reset];
+    self.mediaPlayerController = nil;
+}
+
+#pragma mark Tests
+
 - (void)testSegmentAdditionWhilePlaying
 {
+    [self mpt_expectationForNotification:SRGMediaPlayerPlaybackStateDidChangeNotification object:self.mediaPlayerController handler:^BOOL(NSNotification * _Nonnull notification) {
+        return [notification.userInfo[SRGMediaPlayerPlaybackStateKey] integerValue] == SRGMediaPlayerPlaybackStatePlaying;
+    }];
     
+    [self.mediaPlayerController playURL:OnDemandTestURL() atTime:CMTimeMakeWithSeconds(3., NSEC_PER_SEC) withSegments:nil userInfo:nil];
+    
+    [self waitForExpectationsWithTimeout:30. handler:nil];
+    
+    Segment *segment = [Segment segmentWithTimeRange:CMTimeRangeMake(CMTimeMakeWithSeconds(1., NSEC_PER_SEC), CMTimeMakeWithSeconds(5., NSEC_PER_SEC))];
+    
+    [self mpt_expectationForNotification:SRGMediaPlayerSegmentDidStartNotification object:self.mediaPlayerController handler:^BOOL(NSNotification * _Nonnull notification) {
+        XCTAssertEqualObjects(notification.userInfo[SRGMediaPlayerSegmentKey], segment);
+        XCTAssertNil(notification.userInfo[SRGMediaPlayerPreviousSegmentKey]);
+        XCTAssertFalse([notification.userInfo[SRGMediaPlayerSelectionKey] boolValue]);
+        XCTAssertFalse([notification.userInfo[SRGMediaPlayerSelectedKey] boolValue]);
+        TestAssertEqualTimeInSeconds([notification.userInfo[SRGMediaPlayerLastPlaybackTimeKey] CMTimeValue], 3);
+        return YES;
+    }];
+    
+    self.mediaPlayerController.segments = @[segment];
+    
+    [self waitForExpectationsWithTimeout:30. handler:nil];
 }
 
 - (void)testSegmentRemovalWhilePlaying
