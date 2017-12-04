010117089f8ec635f1f006993583adaf75d64a15
Samuel Défago <defagos@gmail.com>
2017-12-04 12:54:46 +0100

    Update 360 camera with device motions

--- Framework/Sources/Core/SRGMediaPlayback360View.h
+++ Framework/Sources/Core/SRGMediaPlayback360View.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SRGMediaPlayback360View : SCNView <SRGMediaPlaybackView>
+@interface SRGMediaPlayback360View : SCNView <SCNSceneRendererDelegate, SRGMediaPlaybackView>
 
 @end
 
--- Framework/Sources/Core/SRGMediaPlayback360View.m
+++ Framework/Sources/Core/SRGMediaPlayback360View.m
@@ -8,13 +8,23 @@
 
 #import "AVPlayer+SRGMediaPlayer.h"
 
+#import <CoreMotion/CoreMotion.h>
 #import <SpriteKit/SpriteKit.h>
 
 static void commonInit(SRGMediaPlayback360View *self);
 
+@interface SRGMediaPlayback360View ()
+
+@property (nonatomic, weak) SCNNode *cameraNode;
+@property (nonatomic) CMMotionManager *motionManager;
+
+@end
+
 @implementation SRGMediaPlayback360View
 
 @synthesize player = _player;
+
+#pragma mark Object lifecycle
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
@@ -30,6 +40,56 @@ static void commonInit(SRGMediaPlayback360View *self);
         commonInit(self);
     }
     return self;
+}
+
+#pragma mark Overrides
+
+- (void)willMoveToWindow:(UIWindow *)newWindow
+{
+    [super willMoveToWindow:newWindow];
+    
+    if (newWindow) {
+        [self.motionManager startDeviceMotionUpdatesUsingReferenceFrame:CMAttitudeReferenceFrameXArbitraryZVertical];
+    }
+    else {
+        [self.motionManager stopDeviceMotionUpdates];
+    }
+}
+
+#pragma marm SCNSceneRendererDelegate protocol
+
+- (void)renderer:(id<SCNSceneRenderer>)renderer updateAtTime:(NSTimeInterval)time
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        CMAttitude *attitude = self.motionManager.deviceMotion.attitude;
+        if (attitude) {
+            switch ([UIApplication sharedApplication].statusBarOrientation) {
+                case UIInterfaceOrientationPortrait: {
+                    // TODO:
+                    break;
+                }
+                    
+                case UIInterfaceOrientationPortraitUpsideDown: {
+                    // TODO:
+                    break;
+                }
+                    
+                case UIInterfaceOrientationLandscapeLeft: {
+                    self.cameraNode.eulerAngles = SCNVector3Make(attitude.roll + M_PI_2, -attitude.yaw, -attitude.pitch);
+                    break;
+                }
+                    
+                case UIInterfaceOrientationLandscapeRight: {
+                    self.cameraNode.eulerAngles = SCNVector3Make(M_PI_2 - attitude.roll, -attitude.yaw, -attitude.pitch);
+                    break;
+                }
+                    
+                default: {
+                    break;
+                }
+            }
+        }
+    });
 }
 
 #pragma mark SRGMediaPlaybackView protocol
@@ -52,6 +112,7 @@ static void commonInit(SRGMediaPlayback360View *self);
     cameraNode.position = SCNVector3Make(0.f, 0.f, 0.f);
     cameraNode.eulerAngles = SCNVector3Make(M_PI, 0.f, 0.f);
     [scene.rootNode addChildNode:cameraNode];
+    self.cameraNode = cameraNode;
     
     CGSize size = player.srg_assetDimensions;
     SKScene *videoScene = [SKScene sceneWithSize:size];
@@ -73,8 +134,9 @@ static void commonInit(SRGMediaPlayback360View *self);
 
 static void commonInit(SRGMediaPlayback360View *self)
 {
-    // TODO: Too many controls are available we don't want. A custom solution must be implemented.
-    //       Currently there is sometimes a crash because of this camera controller when closing a video (commenting
-    //       out the following line fixes the crash)
-    self.allowsCameraControl = YES;
+    // TODO: It is recommended to have a single instance for the whole app. Move elsewhere
+    self.motionManager = [[CMMotionManager alloc] init];
+    self.motionManager.deviceMotionUpdateInterval = 1. / 60.;
+    
+    self.delegate = self;
 }
