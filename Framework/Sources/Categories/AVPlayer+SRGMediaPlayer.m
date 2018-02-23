eaf6166f14add742818dfb74fefc2f27cc72ff76
Samuel Défago <defagos@gmail.com>
2018-02-23 13:50:44 +0100

    Fix unreliable seek to stream end on iOS 11

--- Framework/Sources/Categories/AVPlayer+SRGMediaPlayer.m
+++ Framework/Sources/Categories/AVPlayer+SRGMediaPlayer.m
@@ -6,6 +6,10 @@
 
 #import "AVPlayer+SRGMediaPlayer.h"
 
+#import <objc/runtime.h>
+
+static void *s_seekRequestsCountKey = &s_seekRequestsCountKey;
+
 @implementation AVPlayer (SRGMediaPlayer)
 
 // TODO: Remove when iOS 10 is the minimum required version.
@@ -17,6 +21,18 @@
     else {
         [self play];
     }
+}
+
+- (void)srg_countedSeekToTime:(CMTime)time toleranceBefore:(CMTime)toleranceBefore toleranceAfter:(CMTime)toleranceAfter completionHandler:(void (^)(BOOL finished, NSInteger pendingSeekCount))completionHandler
+{
+    NSInteger requestCount = [objc_getAssociatedObject(self, s_seekRequestsCountKey) integerValue] + 1;
+    objc_setAssociatedObject(self, s_seekRequestsCountKey, @(requestCount), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
+    [self seekToTime:time toleranceBefore:toleranceBefore toleranceAfter:toleranceAfter completionHandler:^(BOOL finished) {
+        NSInteger pendingSeekCount = [objc_getAssociatedObject(self, s_seekRequestsCountKey) integerValue] - 1;
+        objc_setAssociatedObject(self, s_seekRequestsCountKey, @(pendingSeekCount), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        completionHandler(finished, pendingSeekCount);
+    }];
 }
 
 @end
