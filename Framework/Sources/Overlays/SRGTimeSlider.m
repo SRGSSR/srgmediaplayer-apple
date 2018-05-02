4c4a153de3d72c670c1314f2e4a57008bdbdc381
Samuel Défago <defagos@gmail.com>
2018-05-02 10:06:57 +0200

    Avoid drawing (correct) buffered information away from the knob while seeking

--- Framework/Sources/Overlays/SRGTimeSlider.m
+++ Framework/Sources/Overlays/SRGTimeSlider.m
@@ -436,9 +436,11 @@ static NSString *SRGTimeSliderAccessibilityFormatter(NSTimeInterval seconds)
     [self drawMaximumTrack:context];
     [self drawMinimumTrack:context];
     
-    for (NSValue *value in self.mediaPlayerController.player.currentItem.loadedTimeRanges) {
-        CMTimeRange timeRange = [value CMTimeRangeValue];
-        [self drawBufferringTrackForRange:timeRange context:context];
+    if (self.mediaPlayerController.playbackState != SRGMediaPlayerPlaybackStateSeeking) {
+        for (NSValue *value in self.mediaPlayerController.player.currentItem.loadedTimeRanges) {
+            CMTimeRange timeRange = [value CMTimeRangeValue];
+            [self drawBufferringTrackForRange:timeRange context:context];
+        }
     }
 }
 
