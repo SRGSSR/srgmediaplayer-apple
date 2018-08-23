e0ba4cc51f8aca2b5d0e8335bc515a2db434cc9b
Samuel Défago <defagos@gmail.com>
2018-08-23 09:46:17 +0200

    Improve documentation

--- Framework/Sources/Controllers/SRGMediaPlayerController.h
+++ Framework/Sources/Controllers/SRGMediaPlayerController.h
@@ -207,7 +207,8 @@ NS_ASSUME_NONNULL_BEGIN
  *  thereof). Default is 0 seconds.
  *
  *  @discussion If the distance between the desired playback position and the end is smaller than the maximum tolerated
- *              value according to `endTolerance` and / or `endToleranceRatio`, playback will start at the default position.
+ *              value according to `endTolerance` and / or `endToleranceRatio` (the smallest value wins), playback will
+ *              start at the default position.
  */
 @property (nonatomic) NSTimeInterval endTolerance;
 
@@ -216,7 +217,8 @@ NS_ASSUME_NONNULL_BEGIN
  *  ratio is multiplied with the stream (or segment) duration to calculate the tolerance in seconds. Default is 0.
  *
  *  @discussion If the distance between the desired playback position and the end is smaller than the maximum tolerated
- *              value according to `endTolerance` and / or `endToleranceRatio`, playback will start at the default position.
+ *              value according to `endTolerance` and / or `endToleranceRatio` (the smallest value wins), playback will
+ *              start at the default position.
  */
 @property (nonatomic) float endToleranceRatio;
 
