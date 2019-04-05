7737a7c4e7e5e0d914482c2d77d870dbb6c774e8
Samuel Défago <defagos@gmail.com>
2019-04-05 08:13:29 +0200

    Use OBJC_EXPORT consistently

--- Framework/Sources/Overlays/SRGAirPlayView.h
+++ Framework/Sources/Overlays/SRGAirPlayView.h
@@ -19,7 +19,7 @@ NS_ASSUME_NONNULL_BEGIN
  *  Suggested localized description for the current AirPlay route (if any). Can be used by custom overview implementations
  *  to display a standard route description message.
  */
-OBJC_EXTERN NSString * _Nullable SRGAirPlayRouteDescription(void);
+OBJC_EXPORT NSString * _Nullable SRGAirPlayRouteDescription(void);
 
 /**
  *  AirPlay view delegate protocol, providing optional customization behaviour for the default `SRGAirPlayView`
