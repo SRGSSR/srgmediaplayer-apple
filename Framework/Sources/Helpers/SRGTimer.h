eb1a75ae32f3b7230728c4fcc7d01230a15f384f
Samuel Défago <defagos@gmail.com>
2020-01-06 16:54:51 +0100

    Add custom timer suspended when the application is in background

---
+++ Framework/Sources/Helpers/SRGTimer.h
@@ -0,0 +1,59 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ *  A timer class, similar to `NSTimer`, but pausing updates when the application is sent to the background.
+ */
+@interface SRGTimer : NSObject
+
+/**
+ *  Create a timer. After instantiation, call `-resume` to start the timer.
+ *
+ *  @param interval The interval at which the block must be executed.
+ *  @param repeats  Whether the timer repeats until invalidated.
+ *  @param queue    The serial queue onto which block should be enqueued (main queue if `NULL`).
+ *  @param block    The block to be executed.
+ */
+- (instancetype)initWithInterval:(NSTimeInterval)interval
+                         repeats:(BOOL)repeats
+                           queue:(nullable dispatch_queue_t)queue
+                           block:(void (^)(void))block NS_DESIGNATED_INITIALIZER;
+
+/**
+ *  Resume the timer.
+ */
+- (void)resume;
+
+/**
+ *  Fire the timer, without changing its scheduling. If the timer is non-repeating it will be invalidated afterwards.
+ *
+ *  @discussion The block is called in the queue provided at creation time.
+ */
+- (void)fire;
+
+/**
+ *  Suspend the timer.
+ */
+- (void)suspend;
+
+/**
+ *  Invalidate the timer (which cannot be used anymore afterwards).
+ */
+- (void)invalidate;
+
+@end
+
+@interface SRGTimer (Unavailable)
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Framework/Sources/Helpers/SRGTimer.m
@@ -0,0 +1,140 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import "SRGTimer.h"
+
+#import <libextobjc/libextobjc.h>
+#import <UIKit/UIKit.h>
+
+@interface SRGTimer ()
+
+@property (nonatomic) NSTimeInterval interval;
+@property (nonatomic) BOOL repeats;
+@property (nonatomic) dispatch_queue_t queue;
+@property (nonatomic, copy) void (^block)(void);
+
+@property (nonatomic) dispatch_source_t source;
+@property (nonatomic, getter=isResumed) BOOL resumed;
+
+@end
+
+@implementation SRGTimer
+
+#pragma mark Object lifecycle
+
+- (instancetype)initWithInterval:(NSTimeInterval)interval
+                         repeats:(BOOL)repeats
+                           queue:(dispatch_queue_t)queue
+                           block:(void (^)(void))block
+{
+    if (self = [super init]) {
+        self.interval = interval;
+        self.repeats = repeats;
+        self.queue = queue ?: dispatch_get_main_queue();
+        self.block = block;
+
+        self.source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, self.queue);
+        
+        @weakify(self)
+        dispatch_source_set_event_handler(self.source, ^{
+            @strongify(self)
+            
+            block();
+            if (! repeats) {
+                [self invalidate];
+            }
+        });
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [self invalidate];
+}
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-implementations"
+
+- (instancetype)init
+{
+    [self doesNotRecognizeSelector:_cmd];
+    return [self initWithInterval:0. repeats:NO queue:NULL block:^{}];
+}
+
+#pragma clang diagnostic pop
+
+#pragma mark Timer management
+
+- (void)resume
+{
+    if (self.resumed) {
+        return;
+    }
+    
+    // Use leeway to let the system optimize when timers are fired
+    // See https://developer.apple.com/library/archive/documentation/Performance/Conceptual/EnergyGuide-iOS/MinimizeTimerUse.html
+    // As documented for `NSTimer` tolerance property, a leeway of 10% is a general rule which can significantly improve energy
+    // efficiency.
+    int64_t intervalNsec = self.interval * NSEC_PER_SEC;
+    dispatch_source_set_timer(self.source, dispatch_time(DISPATCH_TIME_NOW, intervalNsec), intervalNsec, intervalNsec / 10.);
+    dispatch_resume(self.source);
+    
+    self.resumed = YES;
+    
+    [NSNotificationCenter.defaultCenter addObserver:self
+                                           selector:@selector(applicationDidEnterBackground:)
+                                               name:UIApplicationDidEnterBackgroundNotification
+                                             object:nil];
+    [NSNotificationCenter.defaultCenter addObserver:self
+                                           selector:@selector(applicationWillEnterForeground:)
+                                               name:UIApplicationWillEnterForegroundNotification
+                                             object:nil];
+}
+
+- (void)fire
+{
+    dispatch_async(self.queue, self.block);
+    if (! self.repeats) {
+        [self invalidate];
+    }
+}
+
+- (void)suspend
+{
+    if (! self.resumed) {
+        return;
+    }
+    
+    dispatch_suspend(self.source);
+    self.resumed = NO;
+}
+
+- (void)invalidate
+{
+    // See https://forums.developer.apple.com/thread/15902
+    if (! self.resumed) {
+        [self resume];
+    }
+    dispatch_source_cancel(self.source);
+    
+    [NSNotificationCenter.defaultCenter removeObserver:self name:UIApplicationDidEnterBackgroundNotification object:nil];
+    [NSNotificationCenter.defaultCenter removeObserver:self name:UIApplicationWillEnterForegroundNotification object:nil];
+}
+
+#pragma mark Notifications
+
+- (void)applicationDidEnterBackground:(NSNotification *)notification
+{
+    dispatch_suspend(self.source);
+}
+
+- (void)applicationWillEnterForeground:(NSNotification *)notification
+{
+    dispatch_resume(self.source);
+}
+
+@end
