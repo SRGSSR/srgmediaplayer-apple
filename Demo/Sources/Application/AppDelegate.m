b5455b40cad960bde3d4abd8b1f1bc755d773e40
Samuel Défago <defagos@gmail.com>
2021-09-01 15:27:29 +0200

    Use scenes on iOS / tvOS 13+

--- Demo/Sources/Application/AppDelegate.m
+++ Demo/Sources/Application/AppDelegate.m
@@ -6,54 +6,28 @@
 
 #import "AppDelegate.h"
 
-#import "MediasViewController.h"
+#import "Application.h"
 
 @import AVFoundation;
-@import SRGLogger;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
-    [self.window makeKeyAndVisible];
-    
     [AVAudioSession.sharedInstance setCategory:AVAudioSessionCategoryPlayback error:NULL];
     
-    MediasViewController *videosViewController = [[MediasViewController alloc] initWithTitle:NSLocalizedString(@"Videos", nil) configurationFileName:@"VideoDemoConfiguration" mediaPlayerType:MediaPlayerTypeStandard];
-    MediasViewController *segmentsViewController = [[MediasViewController alloc] initWithTitle:NSLocalizedString(@"Segments", nil) configurationFileName:@"SegmentDemoConfiguration" mediaPlayerType:MediaPlayerTypeSegments];
-    MediasViewController *audiosViewController = [[MediasViewController alloc] initWithTitle:NSLocalizedString(@"Audios", nil) configurationFileName:@"AudioDemoConfiguration" mediaPlayerType:MediaPlayerTypeStandard];
-    
-#if TARGET_OS_IOS
-    UINavigationController *videosNavigationController = [[UINavigationController alloc] initWithRootViewController:videosViewController];
-    videosNavigationController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Videos", nil) image:[UIImage imageNamed:@"videos"] tag:0];
-    
-    UINavigationController *segmentsNavigationController = [[UINavigationController alloc] initWithRootViewController:segmentsViewController];
-    segmentsNavigationController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Segments", nil) image:[UIImage imageNamed:@"segments"] tag:1];
-    
-    MediasViewController *multiPlayerViewController = [[MediasViewController alloc] initWithTitle:NSLocalizedString(@"Multi-stream", nil) configurationFileName:@"MultiPlayerDemoConfiguration" mediaPlayerType:MediaPlayerTypeMulti];
-    UINavigationController *multiPlayerNavigationController = [[UINavigationController alloc] initWithRootViewController:multiPlayerViewController];
-    multiPlayerNavigationController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Multi-stream", nil) image:[UIImage imageNamed:@"multiplayer"] tag:2];
-    
-    UINavigationController *audiosNavigationController = [[UINavigationController alloc] initWithRootViewController:audiosViewController];
-    audiosNavigationController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Audios", nil) image:[UIImage imageNamed:@"audios"] tag:3];
-    
-    UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    tabBarController.viewControllers = @[ videosNavigationController, segmentsNavigationController, multiPlayerNavigationController, audiosNavigationController ];
-    self.window.rootViewController = tabBarController;
-#else
-    videosViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Videos", nil) image:nil tag:0];
-    segmentsViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Segments", nil) image:nil tag:1];
-    audiosViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:NSLocalizedString(@"Audios", nil) image:nil tag:2];
-    
-    UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    tabBarController.viewControllers = @[ videosViewController, segmentsViewController, audiosViewController ];
-    
-    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:tabBarController];
-    self.window.rootViewController = navigationController;
-#endif
-    
+    if (@available(iOS 13, tvOS 13, *)) {}
+    else {
+        self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+        [self.window makeKeyAndVisible];
+        self.window.rootViewController = ApplicationRootViewController();
+    }
     return YES;
+}
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options API_AVAILABLE(ios(13.0))
+{
+    return [[UISceneConfiguration alloc] initWithName:@"Default" sessionRole:connectingSceneSession.role];
 }
 
 @end
